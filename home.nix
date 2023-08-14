06d15ab85f5bfbca1bbaaf20a1db1f890a31493c
Minghong Xu <86758413+MinghonZi@users.noreply.github.com>
2023-08-14 20:10:24 +0100

    Add Git config

--- home.nix
+++ home.nix
@@ -16,4 +16,10 @@
   };
 
   programs.home-manager.enable = true;
+  
+  programs.git = {
+    enable = true;
+    userName = "Minghong Xu";
+    userEmail = "86758413+MinghonZi@users.noreply.github.com";
+  };
 }
